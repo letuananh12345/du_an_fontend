fefa4eaeaf3ef434260376c995b7ae0e0cfc6dde
letuananh12345 <letuananhmdc6789@gmail.com>
2021-10-06 17:24:41 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HearderComponent } from './hearder/hearder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HearderComponent
   ],
   imports: [
     BrowserModule
